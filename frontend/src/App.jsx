17a49410f94e59c361d6f5994e200bb4a8fe4420
Jessé Azevêdo <jessedanobrega@gmail.com>
2024-01-30 23:17:09 -0300

    Tela de Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,15 @@ const Dashboard = lazy(() => import("./pages/dashboard"));
 
 import Layout from "./layout/Layout";
 import AuthLayout from "./layout/AuthLayout";
+import Login from "./pages/auth/login";
 
 function App() {
   return (
     <main className="App  relative">
       <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="/" element={<Login />} />
+        </Route>
         <Route path="/*" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
         </Route>
